8c3aa9e0ea9ea6092a0875edb405ecc3d52f0f02
Ramón Souza <contato@ramonsouza.com>
2019-01-24 20:49:48 -0200

    rota post para receber registros + instalação body-parser para json

--- routes/pagamentos.js
+++ routes/pagamentos.js
@@ -2,5 +2,11 @@ module.exports = function(app){
     app.get('/pagamentos', function(req,res){
         res.send('OK');
     });
+
+    app.post('/pagamentos/pagamento', function(req,res){
+        var pagamento = req.body;
+        console.log(pagamento);
+        res.send('OK');
+    });
 }
 
